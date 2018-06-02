e8b785375f65637deca4b137d7576d3101d3794b
cavacado <ngzhaoloong@gmail.com>
2018-06-02 13:29:00 +0800

    chpt2 : done

---
+++ chpt2/244.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c;
+  float d, e, f;
+  printf("%d", a);
+  printf("%d", b);
+  printf("%d\n", c);
+
+  printf("%f", d);
+  printf("%f", e);
+  printf("%f\n", f);
+  
+  return 0;
+}
